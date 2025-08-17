d6b168bb3f2133025a5041c8b350aa072e6b430b
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-08-17 15:34:45 +0300

    3.2: puhelinluettelo

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const dateInfo = Date(Date.now())
+  response.send(`<p>Phonebook has info for ${persons.length} people</p> ${dateInfo}`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
